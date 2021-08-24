70e095c500413894966110c4337c4af395b2e9da
Orynbekov Nurlybek <nurlybek.dev@gmail.com>
2021-08-24 17:49:49 +0600

    Add viewsets

--- src/polls/urls.py
+++ src/polls/urls.py
@@ -1,3 +1,11 @@
-urlpatterns = [
-    
-]
+from rest_framework import routers, urlpatterns
+
+from polls.views import PollViewSet, QuestionViewSet, VoteViewSet
+
+
+router = routers.DefaultRouter()
+router.register('polls', PollViewSet)
+router.register('questions', QuestionViewSet)
+router.register('votes', VoteViewSet)
+
+urlpatterns = router.urls
--- src/polls/views.py
+++ src/polls/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .serializers import PollSerializer, QuestionSerializer, VoteSerializer
+from .models import Poll, Question, Vote
+
+
+
+class PollViewSet(viewsets.ModelViewSet):
+    queryset = Poll.objects.all()
+    serializer_class = PollSerializer
+
+
+class QuestionViewSet(viewsets.ModelViewSet):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
+
+
+class VoteViewSet(viewsets.ModelViewSet):
+    queryset = Vote.objects.all()
+    serializer_class = VoteSerializer
+    http_method_names = ('get', 'post')
+
+    def perform_create(self, serializer):
+        if self.request.user.is_authenticated:
+            return serializer.save(user=self.request.user)
+        
+        return super().perform_create(serializer)
