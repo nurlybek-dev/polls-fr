a4faf3318f956d068c08b8cbb2209a2b2e8cc3a6
Orynbekov Nurlybek <nurlybek.dev@gmail.com>
2021-08-24 17:24:14 +0600

    Add models

--- src/polls/models.py
+++ src/polls/models.py
@@ -1,1 +1,53 @@
+import datetime
+from django.conf import settings
 from django.db import models
+
+class Poll(models.Model):
+    title = models.CharField(max_length=256)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    description = models.TextField(max_length=1024)
+
+
+class Question(models.Model):
+    class Type:
+        """
+        Описывает типы вопроса
+        """
+        TEXT = 'TEXT'
+        CHOICE = 'CHOICE'
+        MULTICHOICE = 'MULTICHOICE'
+
+        choices = (
+            (TEXT, 'TEXT'),
+            (CHOICE, 'CHOICE'),
+            (MULTICHOICE, 'MULTICHOICE'),
+        )
+
+    poll = models.ForeignKey(
+        Poll, related_name='questions', on_delete=models.CASCADE
+    )
+    text = models.CharField(max_length=256)
+    type = models.CharField(
+        max_length=11, choices=Type.choices, default=Type.TEXT
+    )
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(
+        Question, related_name='choices', on_delete=models.CASCADE
+    )
+    text = models.CharField(max_length=128, default='Enter value')
+
+
+class Vote(models.Model):
+    poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, blank=True, null=True)
+    date = models.DateField(default=datetime.date.today(), editable=False)
+
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    vote = models.ForeignKey(Vote, related_name='answers', on_delete=models.CASCADE)
+    choice = models.ForeignKey(Choice, on_delete=models.CASCADE)
+    value = models.CharField(max_length=128, blank=True, null=True)
