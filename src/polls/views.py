4f343a1279718f47ccb53b2165e236344b2b09a1
Orynbekov Nurlybek <nurlybek.dev@gmail.com>
2021-08-24 18:10:37 +0600

    Add permissions

--- src/polls/views.py
+++ src/polls/views.py
@@ -1,24 +1,28 @@
 from rest_framework import viewsets
+from rest_framework import permissions
+
 
 from .serializers import PollSerializer, QuestionSerializer, VoteSerializer
 from .models import Poll, Question, Vote
+from .permissions import PollPermission
 
 
 
 class PollViewSet(viewsets.ModelViewSet):
     queryset = Poll.objects.all()
     serializer_class = PollSerializer
+    permission_classes = (PollPermission, )
 
 
 class QuestionViewSet(viewsets.ModelViewSet):
     queryset = Question.objects.all()
     serializer_class = QuestionSerializer
+    permission_classes = (permissions.IsAdminUser, )
 
 
 class VoteViewSet(viewsets.ModelViewSet):
     queryset = Vote.objects.all()
     serializer_class = VoteSerializer
-    http_method_names = ('get', 'post')
 
     def perform_create(self, serializer):
         if self.request.user.is_authenticated:
